212c1510e7f76ee59e70bd99e954beb55ae0c434
shane <40685570@live.napier.ac.uk>
2024-10-01 11:11:18 +0630

    Testmongodb

---
+++ src/main/java/group6/com/napier/devops/App.java
@@ -0,0 +1,37 @@
+package group6.com.napier.devops;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        // Use try-with-resources to ensure MongoClient is closed
+        try (MongoClient mongoClient = new MongoClient("localhost", 27017)) {
+            // Connect to MongoDB on local system - we're using port 27000
+            // Get a database - will create when we use it
+            MongoDatabase database = mongoClient.getDatabase("mydb");
+
+            // Get a collection from the database
+            MongoCollection<Document> collection = database.getCollection("test");
+
+            // Create a document to store
+            Document doc = new Document("name", "Kevin Sim")
+                    .append("class", "DevOps")
+                    .append("year", "2024")
+                    .append("result", new Document("CW", 95).append("EX", 85));
+
+            // Add document to collection
+            collection.insertOne(doc);
+
+            // Check document in collection
+            Document myDoc = collection.find().first();
+            System.out.println(myDoc.toJson());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
